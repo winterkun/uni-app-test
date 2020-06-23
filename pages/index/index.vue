ab79986ab3dbfc3970ed0fe12dc67848b9164722
winterkun <54101914+winterkun@users.noreply.github.com>
2020-06-23 18:52:47 +0800

    initialization

---
+++ pages/index/index.vue
@@ -0,0 +1,75 @@
+<template>
+	<view class="<home_page></home_page>">
+		<view class="">
+			<!-- <input type="text" value="香蕉李子" /> -->
+			<scroll-view
+				class="scroll-container"
+				scroll-x
+				scroll-with-animation
+				v-if="brands.length > 1"
+				:scroll-into-view="'s' + currentIndex"
+			>
+				<view
+					:class="['scroll-item', { active: index == currentId }]"
+					:id="'s' + index"
+					v-for="(item, index) in brands"
+					:key="index"
+					@tap="handleScroll(index)"
+				>
+					{{ item }}
+					<view class="bottom-border" v-if="index == currentId"><!-- 底部红色短border --></view>
+				</view>
+			</scroll-view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			currentId: '' /* 控制被选中 */,
+			currentIndex: '' /* 控制位置 */,
+			title: 'Hello',
+			brands: ['牛仔裤', '羽绒服', '皮鞋', '运动鞋', '跑步鞋', '篮球鞋', '围巾', '西装', '西裤']
+		};
+	},
+	onLoad() {},
+	methods: {}
+};
+</script>
+
+<style lang="scss">
+.scroll-container {
+	white-space: nowrap;
+	height: 80upx;
+	background: #fff;
+	border-bottom: 1upx solid #ccc;
+
+	.scroll-item {
+		text-align: center;
+		display: inline-block;
+		padding: 0 30upx;
+		line-height: 70upx;
+		font-size: 36upx;
+		font-weight: 400;
+		color: #1e1e1e;
+
+		&.active {
+			display: inline-block;
+			font-weight: bold;
+			color: #1e1e1e;
+			font-size: 42upx;
+			border-bottom: 2px solid #ff4f52;
+		}
+	}
+
+	.bottom-border {
+		margin: 0 auto;
+		width: 50upx;
+		height: 10upx;
+		background: rgba(255, 79, 82, 1);
+		border-radius: 5upx;
+	}
+}
+</style>
